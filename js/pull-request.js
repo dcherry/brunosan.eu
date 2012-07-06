2e5078c995a7457a02ef2000f3e3cc8aedfab834
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2012-07-06 13:10:17 -0400

    updated pull request

--- js/pull-request.js
+++ js/pull-request.js
@@ -1,15 +1,14 @@
 ////Idea taken from https://github.com/dfm
 ///https://raw.github.com/dfm/dfm.github.com/master/assets/comments.js
 
-var prose_url = "http://prose.io/#brunosan/brunosan.eu/gh-pages/_posts/";
-var github_url = "https://github.com/brunosan/brunosan.eu/edit/gh-pages/_posts/";
+var prose_url = "http://prose.io/#brunosan/brunosan.eu/master/_posts/";
+var github_url = "https://github.com/brunosan/brunosan.eu/edit/master/_posts/";
 
 $(function() {
 
     $("#pull-request-btn").click(function () {
         var data = $(this).data();
-        var path = data.title.split("/");
-        var url = github_url + data.date + "-" + path[path.length-1] + ".md";
+        var url = github_url + data.filename;
         window.location = url;
         return false;
     });
